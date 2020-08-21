3808b20da58741f6ab9860ad7164ad7ff84d81fb
Daniel Rasmussen <drasmuss@users.noreply.github.com>
2020-08-21 09:39:45 -0300

    Initial commit

---
+++ pytorch_spiking/version.py
@@ -0,0 +1,15 @@
+"""
+We use semantic versioning (see http://semver.org/).
+and conform to PEP440 (see https://www.python.org/dev/peps/pep-0440/).
+'.devN' will be added to the version unless the code base represents
+a release version. Release versions are git tagged with the version.
+"""
+
+name = "pytorch-spiking"
+version_info = (0, 1, 0)  # (major, minor, patch)
+dev = 0  # set to None for releases
+
+version = "{v}{dev}".format(
+    v=".".join(str(v) for v in version_info),
+    dev=(".dev%d" % dev) if dev is not None else "",
+)
