8bce3f45eb2cb105959238a2d66d8b6fb94a8f48
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2020-11-16 12:46:39 -0400

    Apply isort to imports

--- pytorch_spiking/__init__.py
+++ pytorch_spiking/__init__.py
@@ -2,7 +2,7 @@
 
 __copyright__ = "2020-2020, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
-from pytorch_spiking.version import version as __version__
 
-from pytorch_spiking import modules, functional
+from pytorch_spiking import functional, modules
 from pytorch_spiking.modules import Lowpass, SpikingActivation, TemporalAvgPool
+from pytorch_spiking.version import version as __version__
--- pytorch_spiking/tests/test_modules.py
+++ pytorch_spiking/tests/test_modules.py
@@ -4,7 +4,6 @@ import numpy as np
 import pytest
 import torch
 
-
 from pytorch_spiking import modules
 
 
